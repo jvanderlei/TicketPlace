26ceb9dcce38971a2ceaccf6ce2676375fc88204
jvanderlei <joaop.vanderlei@gmail.com>
2022-05-27 18:53:52 -0300

    Advanced in User

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -6,17 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class UserService  {
 
     private final UserDao userDao;
 
     @Autowired
-    public UserService(@Qualifier("fakeDao") UserDao userDao) {
+    public UserService(@Qualifier("postgres") UserDao userDao) {
         this.userDao = userDao;
     }
 
     public int addUser(User user) {
         return userDao.insertUser(user);
     }
+
+    public List<User> getAllUsers() {
+        return userDao.selectAllUsers();
+    }
+
+    public Optional<User> getUserById(UUID id){
+        return userDao.selectUserByID(id);
+    }
+
+    public int deleteUser(UUID id) {
+        return userDao.deleteUserById(id);
+    }
+
+    public int updateUser(UUID id, User newUser) {
+        return userDao.updateUserById(id, newUser);
+    }
+
 }
